bf3ee7ff2654c5e574d088260ff43d4f396a4dd7
Yareslav <80545801+Yareslav@users.noreply.github.com>
2022-04-19 13:32:28 +0300

    preparation

---
+++ src/reducers/mainReducer.js
@@ -0,0 +1,33 @@
+import {createReducer,createAction} from "@reduxjs/toolkit";
+const itemsFromStorage=JSON.parse(localStorage.getItem("data"));
+const initialState={
+	todos:itemsFromStorage ? itemsFromStorage : []
+}
+export const create=createAction("CREATE");
+export const update=createAction("UPDATE");
+export const check=createAction("CHECK");
+export const remove=createAction("REMOVE")
+export default createReducer(initialState,{
+	[create]:(state,payload)=>{
+		state.todos.push({
+			name:payload,
+			checked:false
+		});
+		insertIntoMemory(state);
+	},
+	[update]:(state,payload)=>{
+		state.todos[payload.index].name=payload.name;
+		insertIntoMemory(state);
+	},
+	[check]:(state,payload)=>{
+		state.todos[payload].checked=!state.todos[payload].checked;
+		insertIntoMemory(state);
+	},
+	[remove]:(state,payload)=>{
+		state.todos.splice(payload,1);
+		insertIntoMemory(state);
+	},
+})
+function insertIntoMemory(data) {
+	localStorage.setItem("data",JSON.stringify(data));
+}
